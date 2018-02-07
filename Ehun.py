a2f1166ad7b117a24dfd269f1e09083f44b8281e
Sarehun <33279202+sarehuna@users.noreply.github.com>
2018-02-08 01:59:31 +0800

    Update Ehun.py

--- Ehun.py
+++ Ehun.py
@@ -835,3 +835,33 @@ def bot(op):
                 G.preventJoinByTicket = True
                 ka.updateGroup(G)
               else:
+                G = ka.getGroup(op.param1)
+                ka.kickoutFromGroup(op.param1,[op.param2])
+                G.preventJoinByTicket = False
+                ka.updateGroup(G)
+                Ticket = ka.reissueGroupTicket(op.param1)
+                cl.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                ki.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                kk.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                kc.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                ks.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                ka.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                kb.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                ku.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                ko.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                ke.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                G.preventJoinByTicket = True
+                #cl.updateGroup(G)
+                ka.updateGroup(G)
+                #wait["blacklist"][op.param2] = True
+                
