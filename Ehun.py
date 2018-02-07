0a26f9ce58d30c665eec7f5966099985b00c771a
Sarehun <33279202+sarehuna@users.noreply.github.com>
2018-02-08 03:54:35 +0800

    Update Ehun.py

--- Ehun.py
+++ Ehun.py
@@ -2794,3 +2794,47 @@ def bot(op):
                         ki.leaveGroup(msg.to)
                     except:
                         pass
+            elif msg.text in ["Ojo koyo kuwe2"]:
+                if msg.toType == 2:
+                    ginfo = cl.getGroup(msg.to)
+                    try:
+                        kk.leaveGroup(msg.to)
+                    except:
+                        pass
+            elif msg.text in ["Ojo koyo kuwe3"]:
+                if msg.toType == 2:
+                    ginfo = cl.getGroup(msg.to)
+                    try:
+                        kc.leaveGroup(msg.to)
+                    except:
+                        pass
+    #-------------Fungsi Leave Group Finish---------------#
+    
+    #-------------Fungsi Tag All Start---------------#
+            elif msg.text in ["Tag all","Tagall"]:
+            	 if msg.from_ in admin:
+                  group = cl.getGroup(msg.to)
+                  nama = [contact.mid for contact in group.members]
+
+                  cb = ""
+                  cb2 = ""
+                  strt = int(0)
+                  akh = int(0)
+                  for md in nama:
+                      akh = akh + int(6)
+
+                      cb += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(md)+"},"""
+
+                      strt = strt + int(7)
+                      akh = akh + 1
+                      cb2 += "@nrik \n"
+
+                  cb = (cb[:int(len(cb)-1)])
+                  msg.contentType = 0
+                  msg.text = cb2
+                  msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+cb+']}','EMTVER':'4'}
+
+                  try:
+                      cl.sendMessage(msg)
+                  except Exception as error:
+                      print error
