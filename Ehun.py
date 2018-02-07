820bb370a34dc7285c5a88d78d3bdfe46f2dc556
Sarehun <33279202+sarehuna@users.noreply.github.com>
2018-02-08 01:48:12 +0800

    Update Ehun.py

--- Ehun.py
+++ Ehun.py
@@ -629,3 +629,32 @@ def bot(op):
                 ki.updateGroup(G)
                 wait["blacklist"][op.param2] = True
                 
+            if op.param3 in Amid:
+              if op.param2 in Bmid:
+                G = kk.getGroup(op.param1)
+                G.preventJoinByTicket = False
+                kk.updateGroup(G)
+                Ticket = kk.reissueGroupTicket(op.param1)
+                cl.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                ki.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                kk.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                kc.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                ks.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                ka.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                kb.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                ku.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                ko.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                ke.acceptGroupInvitationByTicket(op.param1,Ticket)
+                time.sleep(0.01)
+                G.preventJoinByTicket = True
+                kk.updateGroup(G)
+              else:
