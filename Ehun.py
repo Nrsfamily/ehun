d3828fb00d6884fad018929acfbaec480866793a
Sarehun <33279202+sarehuna@users.noreply.github.com>
2018-02-08 10:41:38 +0800

    Update Ehun.py

--- Ehun.py
+++ Ehun.py
@@ -5,34 +5,35 @@ from datetime import datetime
 import time,random,sys,json,codecs,threading,glob,re,os,subprocess
 
 satpam = LINETCR.LINE() # Login Pake Akun Kicker Kalian
-#satpam.login(token="token anda")#ganti dgn token kalian
-satpam.login(token="EpWYYMjmEWUba52UlKA1.8nezDhK7HGW9hVtLK4qvGq.4AyYmATuAvCztvMdCZ5xnyhRST1FUx3coUoPztOJtEw=")
+#satpam.login(qr=True)
+satpam.login(token="")
 satpam.loginResult()
 
 cl = LINETCR.LINE() #Luffy
-#cl.login(token="token anda")#ganti dgn token kalian
+#cl.login(qr=True)
 cl.login(token="EpWYYMjmEWUba52UlKA1.8nezDhK7HGW9hVtLK4qvGq.4AyYmATuAvCztvMdCZ5xnyhRST1FUx3coUoPztOJtEw=")
 cl.loginResult()
 
 ki = LINETCR.LINE() #Zorro
-#ki.login(token="token anda")#ganti dgn token kalian
+#ki.login(qr=True)
 ki.login(token="EpWYYMjmEWUba52UlKA1.8nezDhK7HGW9hVtLK4qvGq.4AyYmATuAvCztvMdCZ5xnyhRST1FUx3coUoPztOJtEw=")
 ki.loginResult()
 
 kk = LINETCR.LINE() #Sanji
-#kk.login(token="token anda")#ganti dgn token kalian
+#kk.login(qr=True)
 kk.login(token="EpWYYMjmEWUba52UlKA1.8nezDhK7HGW9hVtLK4qvGq.4AyYmATuAvCztvMdCZ5xnyhRST1FUx3coUoPztOJtEw=")
 kk.loginResult()
 
-kc = LINETCR.LINE() #Ussop
-#kc.login(token="token anda")#ganti dgn token kalian
+kc = LINETCR.LINE()
+#kc.login(qr=True)
 kc.login(token="
-
+kc.loginResult()
+	 
 print "login success Boss"
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-helpMessage =""" ^[One Piece Bot]^
+helpMessage =""" ^[One Ehun Bot]^
 OWNER •Eehun•
 http://line.me/ti/p/~sarehun
 
